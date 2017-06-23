6aabfd4943b78d9968d670117d514f1cb731dc3b
松下勇輝 <Yuki@matsushitaisamuakira-no-MacBook-Pro.local>
2017-06-23 22:44:35 +0900

    大きさの変わるSphereを

---
+++ Assets/Scripts/ScaleSphere.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleSphere : MonoBehaviour {
+
+	[SerializeField] float sizeX = 1;
+	[SerializeField] float sizeY = 2;
+	[SerializeField] float sizeZ = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = new Vector3 (sizeX, sizeY, sizeZ);
+	}
+}
